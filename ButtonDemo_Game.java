e380d8ed887e848824b4ddd954307c6c52e3440f
Matthieu Tapolsky <matthieutapolsky@matthieus-mbp.bc.edu>
2018-11-28 11:42:20 -0500

    added some looks, and a note

--- ButtonDemo_Game.java
+++ ButtonDemo_Game.java
@@ -1,12 +1,14 @@
+import javax.lang.model.util.ElementScanner6;
 import javax.swing.*;
 import java.awt.Color;
 import java.awt.event.ActionListener;
+import java.util.Random;
 import java.awt.event.ActionEvent;
 
 public class ButtonDemo_Game implements  ActionListener{
-    JPanel titlePanel, bubblePanel, LLPanel, buttonPanel;
-    JLabel bubble1, bubble2, bubble3, bubble4, redLabel, ll1, ll2, ar1;
-    JButton backButton;
+    static JPanel titlePanel, bubblePanel, LLPanel, buttonPanel;
+    static JLabel bubble1, bubble2, bubble3, bubble4, redLabel, ll1, ll2, ar1, ar2;
+    static JButton backButton;
 
     public JPanel createContentPane (){
 
@@ -35,19 +37,20 @@ public class ButtonDemo_Game implements  ActionListener{
         bubblePanel.setSize(1200, 250);
         totalGUI.add(bubblePanel);
 
-        bubble1 = new JLabel("1");
+        bubble1 = new JLabel("[1]");
         bubble1.setLocation(0,0);
         bubble1.setSize(30,30);
         bubblePanel.add(bubble1);
-        bubble2 = new JLabel("2");
+        bubble2 = new JLabel("[2]");
         bubble2.setLocation(30,0);
         bubble2.setSize(30,30);
+        // bubble2.setForeground(Color.RED);
         bubblePanel.add(bubble2);
-        bubble3 = new JLabel("3");
+        bubble3 = new JLabel("[3]");
         bubble3.setLocation(60,0);
         bubble3.setSize(30,30);
         bubblePanel.add(bubble3);
-        bubble4 = new JLabel("4");
+        bubble4 = new JLabel("[4]");
         bubble4.setLocation(90,0);
         bubble4.setSize(30,30);
         bubblePanel.add(bubble4);
@@ -66,8 +69,12 @@ public class ButtonDemo_Game implements  ActionListener{
         ar1.setLocation(30,0);
         ar1.setSize(30,30);
         LLPanel.add(ar1);
+        ar2 = new JLabel(" ->");
+        ar2.setLocation(330,0);
+        ar2.setSize(30,30);
+        LLPanel.add(ar2);
         ll2 = new JLabel("Null");
-        ll2.setLocation(120,0);
+        ll2.setLocation(360,0);
         ll2.setSize(50,30);
         LLPanel.add(ll2);
 
@@ -82,8 +89,8 @@ public class ButtonDemo_Game implements  ActionListener{
         // used before. Now each button has an ActionListener which posts
         // its action out when the button is pressed.
         backButton = new JButton("Back");
-        backButton.setLocation(300, 150);
-        backButton.setSize(500, 70);
+        backButton.setLocation(425, 150);
+        backButton.setSize(200, 70);
         backButton.setFont(backButton.getFont().deriveFont(35.0f));
         backButton.addActionListener(this);
         buttonPanel.add(backButton);
@@ -132,12 +139,27 @@ public class ButtonDemo_Game implements  ActionListener{
         frame.setVisible(true);
     }
 
+    private static void runStuff() {
+        Random rand = new Random();
+        int n = rand.nextInt(4) + 1;
+        if(n == 1){
+            bubble1.setForeground(Color.RED);
+        } else if(n ==2){
+            bubble2.setForeground(Color.RED);
+        } else if (n==2){
+            bubble3.setForeground(Color.RED);
+        } else {
+            bubble4.setForeground(Color.RED);
+        }
+    }
+
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
+                runStuff();
             }
         });
     }
